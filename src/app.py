32b92b9ec8bec26e7a2828368c2cc74fd26bbeb7
Sanketh <sankethr98@gmail.com>
2025-05-06 17:32:09 -0500

    edits

--- src/app.py
+++ src/app.py
@@ -24,34 +24,40 @@ app.layout = html.Div([
         ], style = {'text-align': 'center'}),
     html.Div([
         html.H2("1. Choose Your Era", style = {'font-weight': '600', 'display': 'block', 'marginLeft': 'auto', 'marginRight': 'auto', 'height': '50%', 'width': '75%'}),
-        html.P("Is everyone in the conversation, or should older eras be viewed less favorably?", style = {'display': 'block', 'marginLeft': 'auto', 'marginRight': 'auto', 'height': '50%', 'width': '75%'}),
-        dcc.Slider(id='era-slider', min=1.0, max=7.0, step=0.1, value=1.0, marks={
-                                                                                1: {'label': 'Plumbers'},
-                                                                                2: {'label': '2.0'},
-                                                                                3: {'label': '3.0'},
-                                                                                4: {'label': '4.0'},
-                                                                                5: {'label': '5.0'},
-                                                                                6: {'label': '6.0'},
-                                                                                7: {'label': 'Ballers'},
-                                                                                },
-                    tooltip={'always_visible': True, 'placement': 'top', 'style': {'fontsize': '100px', 'font-weight': 'bold'}})
+        html.P("Is everyone in the conversation, or should older eras be viewed less favorably?", style = {'display': 'block', 'marginLeft': 'auto', 'marginRight': 'auto', 'height': '50%', 'width': '75%'})
         ]),
     html.Div([
+        dcc.Slider(id='era-slider', className='era-slider-class', min=1.0, max=7.0, step=0.1, value=1.0, marks={
+                                                                                                                1: {'label': "Plumbers"},
+                                                                                                                2: {'label': '2.0'},
+                                                                                                                3: {'label': '3.0'},
+                                                                                                                4: {'label': '4.0'},
+                                                                                                                5: {'label': '5.0'},
+                                                                                                                6: {'label': '6.0'},
+                                                                                                                7: {'label': "Ballers"},
+                                                                                                                },
+                    tooltip={'always_visible': True, 'placement': 'top', 'style': {'fontsize': '100px', 'font-weight': 'bold'}})
+        ], style = {'display': 'flex', 'align-items': 'center', 'justify-content': 'center'}),
+    html.Div([
         html.H2("2. Evaluate Performance", style = {'font-weight': '600', 'display': 'block', 'marginLeft': 'auto', 'marginRight': 'auto', 'height': '50%', 'width': '75%'}),
-        html.P("This is purely a measure of a player’s production and ability to increase the chances of winning. A win or an MVP does not influence the evaluation of a player’s performance. We don’t have the luxury of the “eye test” in this statistical assessment, so choose which numbers you want to consider.", style = {'display': 'block', 'marginLeft': 'auto', 'marginRight': 'auto', 'height': '50%', 'width': '75%'}),
-        dcc.Slider(id='regular-season-postseason-slider', min = 0, max = 100, step = 1, value = 50, marks = {
-                                                                                                             0: {'label': 'Season'},
-                                                                                                             100: {'label': 'Playoffs'}
-                                                                                                             },
-                   tooltip={'always_visible': False, 'placement': 'top', 'style': {'display': 'block', 'fontsize': '100px', 'font-weight': 'bold', 'marginLeft': 'auto', 'marginRight': 'auto'}}),
-        html.Div(id='regular-season-postseason-slider-output-container', style = {'text-align': 'center', 'font-weight': 'bold'}),
-        dcc.Slider(id='box-score-advanced-analaytics-slider', min = 0, max = 100, step = 1, value = 50, marks = {
-                                                                                                             0: {'label': 'Traditional'},
-                                                                                                             100: {'label': 'Advanced'}
-                                                                                                             },
-                   tooltip={'always_visible': False, 'placement': 'top', 'style': {'display': 'block', 'fontsize': '100px', 'font-weight': 'bold', 'marginLeft': 'auto', 'marginRight': 'auto'}}),
-        html.Div(id='box-score-advanced-analaytics-slider-output-container', style = {'text-align': 'center', 'font-weight': 'bold'})
+        html.P("This is purely a measure of a player’s production and ability to increase the chances of winning. A win or an MVP does not influence the evaluation of a player’s performance. We don’t have the luxury of the “eye test” in this statistical assessment, so choose which numbers you want to consider.", style = {'display': 'block', 'marginLeft': 'auto', 'marginRight': 'auto', 'height': '50%', 'width': '75%'})
         ]),
+    html.Div([
+        dcc.Slider(id='regular-season-postseason-slider', className='regular-season-postseason-slider-class', min = 0, max = 100, step = 1, value = 50, marks = {
+                                                                                                                                                                 0: {'label': "Regular Season"},
+                                                                                                                                                                 100: {'label': "Postseason"}
+                                                                                                                                                                 },
+                   tooltip={'always_visible': False, 'placement': 'top', 'style': {'display': 'block', 'fontsize': '100px', 'font-weight': 'bold', 'marginLeft': 'auto', 'marginRight': 'auto'}})
+        ], style = {'display': 'flex', 'align-items': 'center', 'justify-content': 'center'}),
+    html.Div(id='regular-season-postseason-slider-output-container', style = {'text-align': 'center', 'font-weight': 'bold'}),
+    html.Div([
+        dcc.Slider(id='box-score-advanced-analaytics-slider', className='box-season-advanced-statistics-slider-class', min = 0, max = 100, step = 1, value = 50, marks = {
+                                                                                                                                                                         0: {'label': 'Traditional Box Score'},
+                                                                                                                                                                         100: {'label': 'Advanced Statistics'}
+                                                                                                                                                                         },
+                   tooltip={'always_visible': False, 'placement': 'top', 'style': {'display': 'block', 'fontsize': '100px', 'font-weight': 'bold', 'marginLeft': 'auto', 'marginRight': 'auto'}})
+        ], style = {'display': 'flex', 'align-items': 'center', 'justify-content': 'center'}),
+    html.Div(id='box-score-advanced-analaytics-slider-output-container', style = {'text-align': 'center', 'font-weight': 'bold'}),
     html.Div([
         html.H2("3. Build Your Model", style = {'font-weight': '600', 'display': 'block', 'marginLeft': 'auto', 'marginRight': 'auto', 'height': '50%', 'width': '75%'}),
         html.P("Distribute 100 points across the following criteria:", style = {'display': 'block', 'marginLeft': 'auto', 'marginRight': 'auto', 'height': '50%', 'width': '75%'})
@@ -63,17 +69,17 @@ app.layout = html.Div([
     Output('regular-season-postseason-slider-output-container', 'children'),
     [Input('regular-season-postseason-slider', 'value')]
     )
-def update_regular_season_postseason_output(playoffs_percentage):
-    season = 100 - playoffs_percentage
-    return f"Regular Season: {season}% / Playoffs: {playoffs_percentage}%"
+def update_regular_season_postseason_output(postseason_percentage):
+    season = 100 - postseason_percentage
+    return f"Regular Season: {season}% / Postseason: {postseason_percentage}%"
 
 @app.callback(
     Output('box-score-advanced-analaytics-slider-output-container', 'children'),
     [Input('box-score-advanced-analaytics-slider', 'value')]
     )
-def update_box_score_advanced_analytics_output(advanced_analytics_percentage):
-    box_score = 100 - advanced_analytics_percentage
-    return f"Traditional Box Score: {box_score}% / Advanced Analytics: {advanced_analytics_percentage}%"
+def update_box_score_advanced_statistics_output(advanced_statistics_percentage):
+    box_score = 100 - advanced_statistics_percentage
+    return f"Traditional Box Score: {box_score}% / Advanced Statistics: {advanced_statistics_percentage}%"
 
 
 
